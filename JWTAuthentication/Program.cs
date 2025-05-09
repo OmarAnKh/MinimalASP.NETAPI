ac78e8878b00807a1df82589acd2d985866139a5
Omar Khalili <OmarKhalili810@gmail.com>
2025-05-09 13:39:53 +0300

    adding the login and verifies the validity

--- JWTAuthentication/Program.cs
+++ JWTAuthentication/Program.cs
@@ -1,3 +1,47 @@
-var app = WebApplication.CreateBuilder(args).Build();
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JWTAuthentication.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<JwtTokenGenerator>(sp =>
+{
+    return new JwtTokenGenerator(builder.Configuration);
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    });
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+app.UseAuthentication(); 
+app.UseAuthorization();
+
 app.MapGet("/",()=>"Hello World");
+app.MapGet("/secret", () => "This is protected")
+    .RequireAuthorization();
+
+app.MapPost("/login", ([FromBody] LoginRequest login, JwtTokenGenerator tokenGenerator) =>
+{
+   
+    var token = tokenGenerator.GenerateToken(login.Username);
+    return Results.Ok(new { token });
+});
+
+
 app.Run();
+
+public record LoginRequest(string Username, string Password);
---
+++ JWTAuthentication/Services/JwtTokenGenerator.cs
@@ -0,0 +1,96 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthentication.Services
+{
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private class User
+        {
+            public User (int userId, string userName, string firstName, string lastName)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            public string LastName { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string UserName { get; set; }
+
+            public int UserId { get; set; }
+        }
+        
+        public string GenerateToken(string username)
+        {
+            var user = ValidateUserCredentials("Omar", "Khalili");
+            if (user == null)
+            {
+                return null;
+            }
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+                new Claim("given_name", user. FirstName),
+                new Claim("family_name", user.LastName),
+                new Claim("UserName", user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        public ClaimsPrincipal? ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer =_configuration["Authentication:Issuer"],
+                ValidAudience = _configuration["Authentication:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Authentication:SecretForKey"]))
+            };
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out _);
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private User ValidateUserCredentials(string username, string password)
+        {
+            return new User(
+                1,
+                username ?? "",
+                "Omar",
+                "Khalili"
+                );
+        }
+    }
+}
